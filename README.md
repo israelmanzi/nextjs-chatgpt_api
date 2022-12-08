1a172377262cad13297fad2b56b62e7b5d7b4add
israelmanzi <104481099+israelmanzi@users.noreply.github.com>
2022-12-08 21:07:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-chatgpt_api
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-ac7waz)
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ChatGPT3</title>
+      </Head>
+
+      
+    </>
+  );
+}
